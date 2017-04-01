720caf58d8ea7b130ac99777b71eceb0523888c3
Alon Blumenreich <alon@mail.com>
2017-04-01 12:59:33 +0300

    In the middle of work

--- task0/part2/hexaPrint.c
+++ task0/part2/hexaPrint.c
@@ -1,7 +1,8 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
-void PrintHex(buffer, length){
+void PrintHex(char* buffer, int length){
 
 }
 
@@ -9,7 +10,7 @@ int main(int argc, char** argv){
 	
 	FILE* file;
 	char* filename = argv[1];
-	char byte[1] = {0};
+	char byte[2] = {0, 0};
 
 	file = fopen(filename, "r");
 	if(file == NULL){
@@ -18,8 +19,16 @@ int main(int argc, char** argv){
 		exit(1);
 	}
 
-	fread(byte, 1, 1, file);
-	printf("%s\n", byte);
+	int i;
+	while(!feof(file)){
+
+		fread(byte, sizeof(char), sizeof(char), file);	
+		// for(i=0; i < 10; i++){
+
+		printf("%02x ", byte[0]);
+		// }
+	}
+	puts("");
 
 
 	fclose(file);
