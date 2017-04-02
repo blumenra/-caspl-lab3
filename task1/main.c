ed660520714ba0a366439f9bd7ba954d6a57fb62
Alon Blumenreich <alon@mail.com>
2017-04-02 23:21:57 +0300

    Implemented t2 and located virus on the heap in each iteration

--- task1/main.c
+++ task1/main.c
@@ -2,21 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-void PrintHex(char* buffer, int length){
-
-	int i;
-	for(i=0; i<length; i++){
-		fprintf(stdout, "%02hhX ", buffer[i]);
-	}
-}
-
-
 typedef struct virus virus;
+typedef struct link link;
+
+void PrintHex(char* buffer, int length);
+void list_print(link *virus_list);
+link* list_append(link* virus_list, virus* data);
+void list_free(link *virus_list);
  
 struct virus {
     unsigned short length;
     char name[16];
     char* signature;
+};
+
+struct link {
+    virus *v;
+    link *next;
 };
 
 int main(int argc, char** argv){
@@ -40,7 +42,7 @@ int main(int argc, char** argv){
 	
 	while(fread(sizeByte, 1, 2, file) == 2){
 
-		virus virus;
+		virus* tempVirus = (virus*) malloc(sizeof(virus));
 
 		short len = 0;
 		if(intEndian == 0){
@@ -55,25 +57,101 @@ int main(int argc, char** argv){
 			perror("Invalid endian!");
 			exit(1);
 		}
-		virus.length = len;
-		char* sig = (char*) malloc(sizeof(char)*(virus.length));
+		tempVirus->length = len;
+		char* sig = (char*) malloc(sizeof(char)*(tempVirus->length));
 
-		fread(virus.name, 1, 16, file);
-		fread(sig, 1, virus.length, file);
-		virus.signature = sig;
+		fread(tempVirus->name, 1, 16, file);
+		fread(sig, 1, tempVirus->length, file);
+		tempVirus->signature = sig;
 
 		fprintf(stdout, "%s", "Virus name: ");
-		fprintf(stdout, "%s\n", virus.name);
+		fprintf(stdout, "%s\n", tempVirus->name);
 		fprintf(stdout, "%s", "Virus size: ");
-		fprintf(stdout, "%d\n", virus.length);
+		fprintf(stdout, "%d\n", tempVirus->length);
 		fprintf(stdout, "%s\n", "Signature: ");
-		PrintHex(virus.signature, virus.length);
+		PrintHex(tempVirus->signature, tempVirus->length);
 		puts("\n");
 
-		free(virus.signature);
+		free(tempVirus->signature);
+		free(tempVirus);
 	}
 
 	fclose(file);
 
 	return 0;
+}
+
+
+void PrintHex(char* buffer, int length){
+
+	int i;
+	for(i=0; i<length; i++){
+		fprintf(stdout, "%02hhX ", buffer[i]);
+	}
+}
+
+void list_print(link *virus_list){
+
+	link* currLink = virus_list;
+
+	while(currLink != NULL){
+
+		virus* virus = currLink->v;
+
+		fprintf(stdout, "%s", "Virus name: ");
+		fprintf(stdout, "%s\n", virus->name);
+		fprintf(stdout, "%s", "Virus size: ");
+		fprintf(stdout, "%d\n", virus->length);
+		fprintf(stdout, "%s\n", "Signature: ");
+		PrintHex(virus->signature, virus->length);
+		puts("\n");
+
+		currLink = currLink->next;
+	}
+}
+
+link* list_append(link* virus_list, virus* data){
+
+	if(virus_list == NULL){
+
+		link* new_list = (link*) malloc(sizeof(link));
+		return new_list;
+	}
+
+	//****Add to the beginning
+	link* link1 = (link*) malloc(sizeof(link));
+	link1->v = data;
+	link1->next = virus_list;
+	return link1;
+	//****Add to the beginning
+
+	//****Add to the end
+	// link* currLink = virus_list;
+	// while(currLink.next != NULL){
+	// 	currLink = currLink.next;
+	// }
+
+	// link* link = (link*) malloc(sizeof(link));
+	// link->v = data;
+	// link->next = NULL;
+	// currLink.next = link;
+	// return virus_list;
+	//****Add to the end
+}
+
+void list_free(link *virus_list){
+
+	if(virus_list != NULL){
+
+		link* currLink = virus_list;
+		link* nextLink = virus_list->next;
+		while(nextLink != NULL){
+			free(currLink->v);
+			free(currLink);
+			currLink = nextLink;
+			nextLink = nextLink->next;
+		}
+		free(currLink->v);
+		free(currLink);
+	}
 }
